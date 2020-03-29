9fa65a9836e4204d64d43ae04be800b0c7134484
Mateus Neves <mateus.neves@oobj.com.br>
2020-03-29 10:51:11 -0300

    Testes da aplicação

--- backend/src/database/connection.js
+++ backend/src/database/connection.js
@@ -1,6 +1,8 @@
 const knex = require('knex');
 const configuration = require('../../knexfile');
 
-const connection = knex(configuration.development);
+const environment = process.env.NDOE_ENV === 'test' ? configuration.test : configuration.development;
+
+const connection = knex(environment);
 
 module.exports = connection;
